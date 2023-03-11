1d315420ee6004ee3bbc7f886ab0b766a441dd9c
David Mwangi <david.mathenge98@gmail.com>
2023-03-11 20:40:09 +0300

    add Button component

---
+++ src/components/Button/button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ButtonProps {
+  children?: any;
+  onClick?: () => void;
+}
+
+const Button = ({ children, onClick }: ButtonProps): JSX.Element => (
+  <button className="bg-purple-200 p-10" onClick={onClick}>
+    {children}
+  </button>
+);
+
+Button.defaultProps = {
+  children: "+",
+  onClick: () => {},
+};
+
+export default Button;
