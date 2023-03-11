6c50ad1a7cd6608ce40b06a44e4f673be5f27f9e
David Mwangi <david.mathenge98@gmail.com>
2023-03-11 20:15:52 +0300

    code cleanup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import * as NextImage from "next/image";
+
+// Allow Storybook to handle Next's <Image> component
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
