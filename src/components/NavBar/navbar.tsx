911c0888b024c741bd226cdd7a1eea5a3ea7c2a0
David Mwangi <david.mathenge98@gmail.com>
2023-03-11 21:52:16 +0300

    use next link to fix refresh issue

--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 const NavBar = (): JSX.Element => (
   <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900">
@@ -11,37 +12,37 @@ const NavBar = (): JSX.Element => (
       <div className="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
-            <a
+            <Link
               href="/"
               className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
               aria-current="page"
             >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="/test"
               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Store
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="#"
               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Pricing Rules
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="#"
               className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
             >
               Cart
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
